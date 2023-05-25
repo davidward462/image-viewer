968db45253db946299487b523d649152d8d4b1e0
David Ward <davidmward@mail.com>
2023-05-24 17:40:33 -0700

    added CheckArgumentNum

--- viewer.c
+++ viewer.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 #include <SDL2/SDL_image.h>
+
+bool CheckArgumentNum(int minArgs, int maxArgs)
+{
+    if((argc > minArgs) && (argc < maxArgs))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
 
 int main(int argc, char **argv)
 {

3dd99cf00f4009157b77616c929b99b0899ac235
David Ward <davidmward@mail.com>
2023-05-25 16:50:35 -0700

    first version of Tokenize()

--- viewer.c
+++ viewer.c
@@ -24,6 +24,23 @@ void CheckArgNum(int argc, int requiredArgs)
     }
 }
 
+/*
+ * Split filename into name and extension.
+ *
+ * @param string: string to be tokenized.
+ * @param stringLen: length of string param.
+ * @return tokenList: array of chars containing the filename and extension.
+ *
+ */
+char** Tokenize(char* string, int stringLen)
+{
+    const char* delim = ".";
+    char** tokenList = malloc(stringLen * 2);
+    tokenList[0] = strtok(string, delim);
+    tokenList[1] = strtok(NULL, delim);
+    return tokenList;
+}
+
 int main(int argc, char **argv)
 {
 #ifdef DEBUG
@@ -32,13 +49,32 @@ int main(int argc, char **argv)
 
     CheckArgNum(argc, 2);
 
+    // Store command line argument
     char* filename;
-    filename = malloc( strlen( argv[1] ) + 1 );
+    int stringLen = strlen( argv[1] ) + 1;
+    filename = malloc( stringLen );
     strcpy( filename, argv[1] );
+
+    // make copy
+    char* filenameCopy;
+    filenameCopy = malloc(stringLen);
+    strcpy( filenameCopy, filename);
 
 #ifdef DEBUG
     printf("opening file: %s\n", filename);
 #endif
+    
+    char** tokenList = malloc(stringLen * 2);
+    tokenList = Tokenize(filenameCopy, stringLen);
+
+#ifdef DEBUG
+    printf("filename: %s\n", tokenList[0]);
+    printf("extension: %s\n", tokenList[1]);
+#endif
+
+    // free memory
+    free(filename);
+    free(filenameCopy);
     
 
     return 0;

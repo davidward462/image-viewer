b982ed289217f8ac22b21010ba08ffb2e3b37237
David Ward <davidmward@mail.com>
2023-05-24 17:50:49 -0700

    add comment

--- viewer.c
+++ viewer.c
@@ -4,20 +4,26 @@
 #include <stdbool.h>
 #include <SDL2/SDL_image.h>
 
-bool CheckArgumentNum(int minArgs, int maxArgs)
+/*
+ * Check number of arguments passed to the program.
+ *
+ * @param requiredArgs: value of argc that we want. Must be greater that 0.
+ * @return true if number of arguments met, false otherwise. 
+ */
+bool CheckArgumentNum(int requiredArgs)
 {
-    if((argc > minArgs) && (argc < maxArgs))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    bool result = false;
 
+    if(argc == requiredArgs)
+    {
+        result = true;
+    }
+    
+    return result;
 }
 
 int main(int argc, char **argv)
 {
+    CheckArgumentNum(1)
     return 0;
 }
